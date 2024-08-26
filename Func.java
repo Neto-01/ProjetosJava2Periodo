3aeac6a8ab115a749b11154d4657484c56c36a25
Neto-01 <antoniomgmedeiros@gmail.com>
2024-08-26 15:20:16 -0300

    Add files via upload

---
+++ Func.java
@@ -0,0 +1,21 @@
+//public class Func{
+//    public static void main(String[] args){
+
+        Calculadora cal = new Calculadora();
+        double resultado = cal.soma(10,5);
+        System.out.println(resultado);
+        
+        double resultado2 = cal.substracao(10,5);
+        System.out.println(resultado2);
+
+        double resultado3 = cal.multiplicacao(10,5);
+        System.out.println(resultado3);
+
+        double resultado4 = cal.divisao(10,5);
+        System.out.println(resultado4);
+
+        
+
+
+//    }
+//}
