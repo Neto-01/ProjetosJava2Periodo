fec0544ab3a17df1bc875f854cd46f8dad677371
Neto-01 <antoniomgmedeiros@gmail.com>
2024-08-26 21:13:01 -0300

    Add files via upload

---
+++ calculadora.java
@@ -0,0 +1,44 @@
+import java.util.Scanner; 
+public class calc {
+  public static void main(String[] args){
+  
+  double valorUm;
+  double valorDois;
+  double somar(double valorUm, double valorDois) {
+    return valorUm + valorDois; 
+  }
+
+    double subtracao(double valorUm, double valorDois) {
+    return = valorUm - valorDois;
+  }
+
+    double multiplicacao(double valorUm, double valorDois){
+    return valorUm * valorDois;
+  }
+
+    double divisao(double valorUm, double valorDois){
+    return valorUm / valorDois;
+  }
+
+  public class main{
+    public static void main(String[] args){
+  
+  Calculadora cal = new Calculadora();
+
+        double resultado = cal.soma(10,5);
+        System.out.println(resultado);
+        
+        double resultado2 = cal.substracao(10,5);
+        System.out.println(resultado2);
+
+        double resultado3 = cal.multiplicacao(10,5);
+        System.out.println(resultado3);
+
+        double resultado4 = cal.divisao(10,5);
+        System.out.println(resultado4);
+  }
+  }
+
+ }
+
+ }
