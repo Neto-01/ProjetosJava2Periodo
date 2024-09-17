083f3708d6b908862b0abb3307ccc4dfbdfde7be
Neto-01 <antoniomgmedeiros@gmail.com>
2024-09-17 00:02:19 -0300

    Parte final

--- Pessoa.java
+++ Pessoa.java
@@ -11,9 +11,19 @@ public class Pessoa
 	private String corOlho;
 	private double salario;
 	
-	Pessoa(String nome, String sobrenome){
+	Pessoa(String nome, String sobrenome, String email, int idade, String cpf, String endereco, double altura, String corCabelo,
+	String corOlho, double salario){
 	    this.nome = nome;
 	    this.sobrenome = sobrenome;
+	    this.email = email;
+	    this.idade = idade;
+	    this.cpf = cpf;
+	    this.endereco = endereco;
+	    this.altura = altura;
+	    this.corCabelo = corCabelo;
+	    this.corOlho = corOlho;
+	    this.salario = salario;
+	    
 	}
 	
 	public void setnome(String nome){
@@ -24,5 +34,29 @@ public class Pessoa
     }
      public String getSobrenome() {
         return sobrenome;
+     }
+     public String getEmail() {
+        return email;
+     }
+     public int getIdade() {
+        return idade;
+     }
+     public String getCpf() {
+        return cpf;
+     }
+     public String getEndereco() {
+        return endereco;
+     }
+     public double getAltura() {
+        return altura;
+     }
+     public String getCorCabelo() {
+        return corCabelo;
+     }
+     public String getCorOlho() {
+        return corOlho;
+     }
+     public double getSalario() {
+        return salario;
      }
 }
--- Pessoa2.java
+++ Pessoa2.java
@@ -2,8 +2,30 @@ public class Pessoa2{
     
     public static void main(String[] args){
         
-        Pessoa jose = new Pessoa("Jose", "Silva");
+        Pessoa jose = new Pessoa("Jose", "Silva", "Josess@gmail.com", 30, "123.543.456-09", "Rua, zeze", 1.72, "Castanho",
+        "Castanho", 5.60490);
         System.out.println(jose.getNome());
         System.out.println(jose.getSobrenome());
+        System.out.println(jose.getEmail());
+        System.out.println(jose.getIdade());
+        System.out.println(jose.getCpf());
+        System.out.println(jose.getEndereco());
+        System.out.println(jose.getAltura());
+        System.out.println(jose.getCorCabelo());
+        System.out.println(jose.getCorOlho());
+        System.out.println(jose.getSalario() + "\n");
+        
+        Pessoa joao = new Pessoa("Joao", "Ferreira", "Joaofr@gmail.com", 32, "432.543.456-09", "Rua, Centro", 1.75, "Castanho","Castanho", 6.60490);
+        
+        System.out.println(joao.getNome());
+        System.out.println(joao.getSobrenome());
+        System.out.println(joao.getEmail());
+        System.out.println(joao.getIdade());
+        System.out.println(joao.getCpf());
+        System.out.println(joao.getEndereco());
+        System.out.println(joao.getAltura());
+        System.out.println(joao.getCorCabelo());
+        System.out.println(joao.getCorOlho());
+        System.out.println(joao.getSalario());
     }
 }
